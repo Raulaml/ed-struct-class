bf2b74d1dd06fb42439da70ed615cba8cefdfa20
Raulaml <rayulusmoreno@gmail.com>
2020-09-03 10:01:40 -0700

    first commit

---
+++ include/Character.hh
@@ -0,0 +1,25 @@
+#include<iostream>
+
+class Character
+{
+    private:
+        short int health;
+        uint32_t strength;
+        uint32_t exp;
+        short int luck;
+        std::string lore; //char en c++
+        uint32_t speed;
+    public:
+        Character(short int, uint32_t, uint32_t, short int, uint32_t); //constructor
+        ~Character(); //destructor
+        short int GetHealth() const;
+        uint32_t GetStrenght() const;
+        uint32_t GetExp() const;
+        short int GetLuck() const;
+        std::string GetLore() const;
+        uint32_t GetSpeed() const;
+
+        void SetLore(std::string);
+        void GetDamage(unsigned int damage);
+};
+
